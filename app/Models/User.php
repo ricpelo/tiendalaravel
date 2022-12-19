d34762302e2ce8a0d50f562c086533d29892ca14
Ricardo Pérez <ricpelo@gmail.com>
2022-12-19 19:31:24 +0100

    Facturas y su relación con usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function facturas()
+    {
+        return $this->hasMany(Factura::class);
+    }
 }
